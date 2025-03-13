e40b8adf5a236e7cdbd8a71e5dd52929fee42e7d
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2025-03-13 02:15:31 -0300

    Criação de inimigos, dano e plataformas que caem

--- scripts/hitbox_player.gd
+++ scripts/hitbox_player.gd
@@ -1,11 +1,18 @@
-extends Node2D
-@onready var player := $player as CharacterBody2D
-@onready var camera := $camera as Camera2D
+extends Area2D
+@onready var anim: AnimationPlayer = $"../anim"
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	player.follow_camera(camera)
-	
+	pass # Replace with function body.
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.name == "hurtbox":
+		anim.play("hurt")
+		
+		
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,34 +1,30 @@
 extends CharacterBody2D
 
+@onready var remote_trasnform := $remote as RemoteTransform2D
+@onready var ray_right: RayCast2D = $ray_right
+@onready var ray_left: RayCast2D = $ray_left
+@onready var ray_down: RayCast2D = $ray_down
+@onready var ray_top: RayCast2D = $ray_top
+
 const SPEED = 80.0
 const JUMP_VELOCITY = -280.0
 const FIREBALL := preload("res://item/fire_ball.tscn")
-const ATTACK_DELAY = 0.8  # Tempo entre ataques
 
 var is_attacking := false
-var attack_timer = 0.0
+var player_life := 100
+var knockback_vector := Vector2.ZERO
+var is_hurted := false
+
 
 func _physics_process(delta: float) -> void:
-	
 	# Adicionar gravidade.
 	if !is_on_floor():
 		velocity += get_gravity() * delta
-		
-	else:
-		# Se o personagem não estiver atacando, decidir entre "walk" ou "idle"
-		if velocity.x != 0 and !is_attacking:
-			$anim.play("run")
-		elif !is_attacking:
-			if is_on_floor() and Input.is_action_just_pressed("down"):
-				$anim.play("lading")
-			else:
-				$anim.play("idle")
+
 
 	# Lidar com o pulo.
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = JUMP_VELOCITY
-		if !is_attacking:
-			$anim.play("jump")  # Animação de pulo
 
 	# Inicia o ataque se o botão for pressionado e não estiver atacando.
 	if Input.is_action_just_pressed("attack") and is_on_floor() and !is_attacking:
@@ -46,28 +42,45 @@ func _physics_process(delta: float) -> void:
 		$texture.flip_h = false
 		if sign($fireball_point.position.x) == -1:
 			$fireball_point.position.x *= -1
-			
 	elif velocity.x < 0:
 		$texture.flip_h = true
 		if sign($fireball_point.position.x) == 1:
 			$fireball_point.position.x *= -1
 
-	move_and_slide()
-	set_animation()
-	
-func set_animation():
-	var state := "idle"
-	
-	if !is_on_floor():
-		state = "jump"
+	if knockback_vector != Vector2.ZERO:
+		velocity = knockback_vector
 		
-	elif velocity.x != 0:
-		state = "run"
+	for platforms in get_slide_collision_count():
+		var collision = get_slide_collision(platforms)
+		
+		if collision.get_collider().has_method("has_collided_with"):
+			collision.get_collider().has_collided_with(collision, self)
+		
+	move_and_slide()
+	set_state()
+
+# Função para decidir qual animação será exibida
+func set_state():
+	var state := "idle"
 	
 	if is_attacking:
 		state = "attack"
-		
-	if $anim.assigned_animation != state:
+
+	elif is_hurted:
+		state = "hurt"
+	
+	elif !is_on_floor():
+		state = "jump"
+
+	# Se a tecla "down" for pressionada enquanto está no chão, animação de "down"
+	elif Input.is_action_just_pressed("down") and is_on_floor():
+		state = "landing"
+
+	# Se o personagem está se movendo, animação de "run"
+	elif velocity.x != 0:
+		state = "run"	
+
+	if $anim.name != state:
 		$anim.play(state)
 
 # Função chamada para realizar o ataque após 0.6 segundos
@@ -81,3 +94,48 @@ func cast_fireball():
 	fireball_instance.position = $fireball_point.global_position
 	await($anim)
 	is_attacking = false
+
+func _on_hurtbox_body_entered(body: Node2D) -> void:
+	#if body.is_in_group("enemies"):
+		#queue_free()
+	var direction := Input.get_axis("left", "right")
+	
+	if player_life <= 0:
+		queue_free()
+	else:
+		if ray_right.is_colliding():
+			take_damage(Vector2(-150,-150))
+			
+		if ray_left.is_colliding():
+			take_damage(Vector2(150,-150))
+			
+		if ray_down.is_colliding():
+			if direction > 0:
+				take_damage(Vector2(-150,-150))
+			else:
+				take_damage(Vector2(150,-150))
+				
+		if ray_top.is_colliding():
+			if direction > 0:
+				take_damage(Vector2(-150,-150))
+			else:
+				take_damage(Vector2(150,-150))		
+func follow_camera(camera):
+	var camera_path = camera.get_path()
+	remote_trasnform.remote_path =camera_path
+	
+func take_damage(knockback_force := Vector2.ZERO, duration := 0.25):
+	player_life -= 1
+	
+	if knockback_force != Vector2.ZERO:
+		
+		knockback_vector = knockback_force
+		
+		var knockback_tween := get_tree().create_tween()
+		knockback_tween.parallel().tween_property(self, "knockback_vector", Vector2.ZERO, duration)
+		$texture.modulate = Color(1,0,0,1)
+		knockback_tween.parallel().tween_property($texture, "modulate", Color(1,1,1,1), duration)
+	
+	is_hurted = true
+	await get_tree().create_timer(0.7).timeout
+	is_hurted = false
