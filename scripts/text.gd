502d480779b2154d6059826c2166f20551bdbfaf
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2025-06-12 18:09:04 -0300

    :bug: fix : Correção da exibição de mensagens no mapa

--- scripts/text.gd
+++ scripts/text.gd
@@ -4,26 +4,23 @@ extends Node2D
 @onready var area: Area2D = $area
 
 const lines : Array[String] = [
-	"Aperte enter ou space...",
+	"Aperte enter para prosseguir...",
 	"Olá, aventureiro!",
 	"Utilize as -> setas para andar.",
 	"Utilize espaço para atirar bolas de fogo.",
-	"Espero que esteja preparado...",
-	"A jornada está apenas começando...",
-	"Boa sorte!!!",
 	"E não chegue perto das muriçocas!",
 	"É sério, não chegue perto delas!",
+	"Boa sorte!!!",
 ]
 
 func _unhandled_input(event: InputEvent) -> void:
 	if area.get_overlapping_bodies().size() > 0:
 		texture.show()
-		if event.is_action_pressed("start"):
-			if !DialogManager.is_message_active:
-				texture.hide()
-				DialogManager.start_message(global_position, lines)
-			elif DialogManager.can_advance_message:
-				DialogManager.next_line()
+		if !DialogManager.is_message_active:
+			texture.hide()
+			DialogManager.start_message(global_position, lines)
+		elif DialogManager.can_advance_message:
+			DialogManager.next_line()
 	else:
 		texture.hide()
 		if DialogManager.dialog_box != null:
